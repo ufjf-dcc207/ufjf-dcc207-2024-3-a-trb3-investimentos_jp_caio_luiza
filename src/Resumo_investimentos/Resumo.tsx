caeb3d653e116c81410f94d3b1599f842833c09f
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-02-10 15:17:44 -0300

    Cria tabela do resumo dos investimentos e adiciona os cálculos

---
+++ src/Resumo_investimentos/Resumo.tsx
@@ -0,0 +1,38 @@
+//import { CalculadoraProps } from "./Calculadora";
+import "./Resumo.css";
+
+interface CalculadoraProps {
+    valorTotal: number;
+    valorInvestido: number;
+    valorImposto: number;
+}
+
+export default function Resumo({ valorTotal, valorInvestido, valorImposto }: CalculadoraProps) {
+    return (
+        <div className="resumo">
+            <table className="tabela">
+            <caption className="titulo">Resumo do Investimento</caption>
+            <tr>
+                <td>Dinheiro Investido:</td>
+                <td className="direita">{valorInvestido.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+            </tr>
+            <tr>
+                <td>Seu  dinheiro renderá:</td>
+                <td className="direita">{((valorTotal - valorInvestido)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+            </tr>
+            <tr>
+                <td>Total Bruto:</td>
+                <td className="direita">{valorTotal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+            </tr>
+            <tr>
+                <td>Valor do Imposto:</td>
+                <td className="direita">{(valorImposto * (valorTotal-valorInvestido)).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+            </tr>
+            <tr className="total">
+                <td>Total Líquido:</td>
+                <td className="direita">{(valorTotal-(valorImposto * (valorTotal-valorInvestido))).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+            </tr>
+            </table>
+        </div>
+    );
+}
