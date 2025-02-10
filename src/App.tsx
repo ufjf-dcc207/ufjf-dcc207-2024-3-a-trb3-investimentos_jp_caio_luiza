1ba22d7b8386d18465a0aa346b12adcb7b7da387
Luiza Caldeira Daniel <luiza.caldeira@estudante.ufjf.br>
2025-02-10 15:34:39 -0300

    Adiciona o componente resumo no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Footer from './components/Footer'
 import Navbar from './components/Navbar';
+import Resumo from './Resumo_investimentos/Resumo';
 
 export default function App() {
   return (
     <div className="app"> 
       <Navbar />
+      <Resumo valorInvestido={1000} valorTotal={1100} valorImposto={0.10}/>
       <Footer />
     </div>
   );
