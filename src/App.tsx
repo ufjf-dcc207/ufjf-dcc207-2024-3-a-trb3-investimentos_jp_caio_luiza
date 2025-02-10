b0edd005bf0d934e96b5075ec3d68d38e57a6b87
Caio Reis <caiofreis2005@gmail.com>
2025-02-10 13:27:19 -0300

    Faz o componente Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Footer from './components/Footer'
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <div className="app"> 
+      <Navbar />
       <Footer />
     </div>
   );
