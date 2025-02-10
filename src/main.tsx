aa8d6225b3defb974c7cb84ce6ad04c034da5bc8
Caio Reis <caiofreis2005@gmail.com>
2025-02-10 13:14:50 -0300

    Faz alteração no main.tsx, removendo o import direto do Footer

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import Footer from './components/Footer/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
